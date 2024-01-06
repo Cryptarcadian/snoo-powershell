eedc3f81878dbbaa209aac228f704438354d6a78
Stephen Mok <me@stephen.is>
2024-01-07 02:15:18 +1100

    Add basic data retrieval functions

--- snoo-powershell.ps1
+++ snoo-powershell.ps1
@@ -36,3 +36,64 @@ function Request-SnooToken {
 		}
 	}
 }
+
+function Get-SnooData {
+	[CmdletBinding()]
+	param([SecureString]$Token, [Uri]$Endpoint, [Switch]$AsJson, $Query)
+	if (!$Token) {
+		$Token = (Request-SnooToken | ConvertTo-SecureString -AsPlainText)
+	}
+	if (!$Endpoint) {
+		$Endpoint = 'analytics/sessions/last'
+	}
+	$RequestParams = @{
+		UserAgent = 'SNOO/351 CFNetwork/1121.2 Darwin/19.2.0'
+		Method = 'GET'
+		Authentication = 'Bearer'
+		SessionVariable = 'Session'
+		Token = $Token
+		Uri = 'https://snoo-api.happiestbaby.com/' + $Endpoint
+		Body = $Query
+	}
+	$Response = Invoke-RestMethod @RequestParams
+	if ($AsJson) {
+		$Response | ConvertTo-Json
+	} else {
+		$Response
+	}
+}
+
+function Get-SnooDevice {
+	[CmdletBinding()]
+	param([Switch]$AsJson)
+	Get-SnooData -Endpoint 'me/devices' -AsJson:$AsJson
+}
+
+function Get-SnooAccount {
+	[CmdletBinding()]
+	param([Switch]$AsJson)
+	Get-SnooData -Endpoint 'us/me' -AsJson:$AsJson
+}
+
+function Get-SnooBaby {
+	[CmdletBinding()]
+	param([Switch]$AsJson)
+	Get-SnooData -Endpoint 'us/v3/me/baby' -AsJson:$AsJson
+}
+
+function Get-SnooDeviceConfig {
+	[CmdletBinding()]
+	param([Switch]$AsJson)
+	Get-SnooData -Endpoint ('ds/devices/' + (Get-SnooDevice).serialNumber  + '/configs') -AsJson:$AsJson
+}
+
+function Get-SnooSessionsDaily {
+	[CmdletBinding()]
+	param([Switch]$AsJson, $StartTime)
+	$Query = @{
+		'levels' = 'true'
+		'detailedLevels' = 'true'
+		'startTime' = (Get-Date $StartTime -Format 'O')
+	}
+	Get-SnooData -EndPoint ('ss/v2/babies/' + (Get-SnooBaby)."_id" + '/sessions/aggregated/daily') -Query $Query
+}
